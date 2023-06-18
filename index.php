55ae256391a88a12609d662b20d491af17ed1504
javi <ingenierodesoftware73@gmail.com>
2023-06-17 20:45:10 -0500

    code to solve error with charset

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
+<?php
+/**
+     * Open the database connection with the credentials from application/config/config.php
+     */
+    private function openDatabaseConnection()
+    {
+        $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
 
+        // setting the encoding is different when using PostgreSQL
+        if (DB_TYPE == "pgsql") {
+                $databaseEncodingenc = " options='--client_encoding=" . DB_CHARSET . "'";
+        } else {
+                $databaseEncodingenc = "; charset=" . DB_CHARSET;
+        }
 
-//un simple hola mundo
-echo "Hola Mundo";
+        // generate a database connection, using the PDO connector
+        // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
+        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . $databaseEncodingenc, DB_USER, DB_PASS, $options);
+    }
